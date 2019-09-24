f250d271df910afca48ea3a4d7c89af4976ae742
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-24 21:55:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,5 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
-  data.each do |k, v|
-     k.each do |pigeon_value, pigeon_name|
-      pigeon_name.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][v] ||= []
-        pigeon_list[name][v].push(k.to_s)
-      end
-    end
-  end
-  pigeon_list
+  
 end
